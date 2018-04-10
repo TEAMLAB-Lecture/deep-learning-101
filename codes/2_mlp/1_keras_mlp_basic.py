ac2ca600524c68ffaf8e660f2eafd797135d462e
Sungchul Choi <blissray@gmail.com>
2018-04-10 22:13:00 +0900

    Add keras examples

---
+++ codes/2_mlp/1_keras_mlp_basic.py
@@ -0,0 +1,76 @@
+
+
+from __future__ import print_function
+
+from keras.utils import np_utils
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+
+
+batch_size = 128
+num_classes = 10
+epochs = 20
+
+
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+
+
+x_axis, y_axis = x_train[0].shape
+
+
+# import matplotlib.pyplot as plt
+# plt.gray()
+# plt.imshow(x_train[0])
+# plt.show()
+
+
+x_train = x_train.reshape(60000, x_axis*y_axis)
+x_test = x_test.reshape(10000, x_axis*y_axis)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+
+x_train /= 255 #Normalization
+x_test /= 255
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+
+
+x_train[0]
+
+
+
+import keras
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+
+model = Sequential()
+model.add(Dense(64, activation='relu', input_shape=(784,)))
+model.add(Dropout(0.2))
+model.add(Dense(10, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer="sgd",
+              metrics=['accuracy'])
+
+history = model.fit(x_train, y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=(x_test, y_test))
+
+score = model.evaluate(x_test, y_test, verbose=0)
+
+
+
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
---
+++ codes/2_mlp/environment.md
@@ -0,0 +1,42 @@
+### 파이썬 가상환경 설정
+conda create -n dl python=3.6
+```bash
+activate dl
+```
+
+### 파이썬 머신러닝 에코시스템 설치
+```bash
+conda install -y jupyter          #Machine Learning toolkits
+conda install -y scipy
+conda install -y pandas
+conda install -y numpy
+conda install -y scikit-learn    
+# conda install -c anaconda graphviz
+conda install -y seaborn
+conda install -y matplotlib                
+conda install -y graphviz           #Visualization
+```
+#### Install GRAPHVIZ_DOT
+Download & install
+- https://graphviz.gitlab.io/_pages/Download/windows/graphviz-2.38.msi
+
+### 파이썬 머신러닝 에코시스템 설치
+```bash
+conda install -y tensorflow
+conda install -c conda-forge keras
+```
+
+### Keras Setup
+```bash
+where python # See Your
+cd %USERPROFILE%\AppData\Local\conda\conda\envs\dl\etc\conda\activate.d
+atom keras_activate.bat
+```
+
+
+#### Change `KERAS_BACKEND`
+```bash
+set "KERAS_BACKEND=tensorflow"
+set "GRAPHVIZ_DOT=C:\Program Files (x86)\Graphviz2.38\bin\dot.exe"
+set "PATH=%PATH%;C:\Program Files (x86)\Graphviz2.38\bin"
+```
