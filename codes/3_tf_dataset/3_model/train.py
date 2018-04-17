d88f69f2193a72324134f9dbdd0534903bb0edc2
Sungchul Choi <sc82.choi@gachon.ac.kr>
2018-04-18 00:56:30 +0900

    update 3_model/train.py

--- codes/3_tf_dataset/3_model/train.py
+++ codes/3_tf_dataset/3_model/train.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
 from model import MLP
-import tensorflow as tf
 import numpy as np
 
 
@@ -25,7 +24,6 @@ def train(config_dict):
     X = tf.placeholder(tf.float32, [None, 28*28])
     Y = tf.placeholder(tf.float32, [None, 10])
 
-
     n_in = 28*28
     n_hiddens = config_dict["n_hiddens"]
     n_out = 10
@@ -34,27 +32,41 @@ def train(config_dict):
     loss = loss_function(mlp_model.hypothesis, Y)
     train_step = training(loss, learning_rate=config_dict["learning_rate"])
 
+    train_data = np.load("notMNIST_large_train.npy")
+    train_features = train_data["features"]
+    train_labels = train_data["labels"]
 
-    data = np.load("notMNIST_large.npy")
-    features = data["features"]
-    labels = data["labels"]
+    train_dataset = tf.data.Dataset.from_tensor_slices(
+                                        (train_features, train_labels))
+    train_dataset = train_dataset.shuffle(buffer_size=10000)
+    train_dataset = train_dataset.batch(config_dict["batch_size"])
 
-    dataset = tf.data.Dataset.from_tensor_slices((features, labels))
-    dataset = dataset.shuffle(buffer_size=10000)
-    dataset = dataset.batch(config_dict["batch_size"])
-
-    iterator = tf.data.Iterator.from_structure(dataset.output_types,
-                                            dataset.output_shapes)
+    iterator = tf.data.Iterator.from_structure(
+                            train_dataset.output_types,
+                            train_dataset.output_shapes)
     next_element = iterator.get_next()
-    init_op = iterator.make_initializer(dataset)
+    train_init_op = iterator.make_initializer(train_dataset)
 
+    test_data = np.load("notMNIST_large_test.npy")
+    test_features = test_data["features"]
+    test_labels = test_data["labels"]
+    test_dataset = tf.data.Dataset.from_tensor_slices(
+                                        (test_features, test_labels))
+
+    test_dataset = test_dataset.batch(test_labels.shape[0])
+
+    test_iterator = tf.data.Iterator.from_structure(
+                            test_dataset.output_types,
+                            test_dataset.output_shapes)
+    test_next_element = test_iterator.get_next()
+    test_init_op = test_iterator.make_initializer(test_dataset)
 
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
 
     for epoch in range(config_dict["training_epochs"]):
             # initialize the iterator on the training data
-            sess.run(init_op)
+            sess.run(train_init_op)
 
             # get each element of the training dataset until the end is reached
             cost = 0
@@ -78,11 +90,22 @@ def train(config_dict):
             print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.9f}'.format(
                     avg_cost))
 
-            correct_prediction = tf.equal(
-                    tf.argmax(mlp_model.hypothesis, axis=1), tf.argmax(Y, axis=1))
-            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-            print('Accuracy:', sess.run(accuracy, feed_dict={
-                  X: features, Y: labels}))
+            sess.run(test_init_op)
+            while True:
+                try:
+                    batch = sess.run(test_next_element)
+                    batch_x = batch[0]
+                    batch_y = batch[1]
+
+                    correct_prediction = tf.equal(
+                            tf.argmax(mlp_model.hypothesis, axis=1),
+                            tf.argmax(Y, axis=1))
+                    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+                    print('Accuracy:', sess.run(accuracy, feed_dict={
+                          X: batch_x, Y: batch_y}))
+                except tf.errors.OutOfRangeError:
+                    break
+
 
 if __name__ == "__main__":
     config_dict = {
